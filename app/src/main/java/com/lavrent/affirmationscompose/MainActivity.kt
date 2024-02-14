3a2c33915d48531781e8f83b506d3e9843775551
pavlav <pa.lavrentyev@gmail.com>
2024-02-14 14:59:39 +0300

    Теперь вы знаете, как создавать карточки, элементы списка и прокручиваемые списки с помощью Jetpack Compose! Имейте в виду, что это всего лишь базовые инструменты для создания списка. Вы можете дать волю своему творчеству и настраивать элементы списка так, как вам нравится!
    Используйте Card составные файлы для создания элементов списка.
    Измените пользовательский интерфейс, содержащийся в Card составном файле.
    Создайте прокручиваемый список с помощью LazyColumn составного элемента.
    Создайте список, используя пользовательские элементы списка.

--- app/src/main/java/com/lavrent/affirmationscompose/MainActivity.kt
+++ app/src/main/java/com/lavrent/affirmationscompose/MainActivity.kt
@@ -3,11 +3,28 @@ package com.lavrent.affirmationscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lavrent.affirmationscompose.data.Datasource
+import com.lavrent.affirmationscompose.model.Affirmations
 import com.lavrent.affirmationscompose.ui.theme.AffirmationsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,4 +47,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
+    AffirmationList(
+        affirmationList = Datasource().loadAffirmations(),
+        )
+}
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmations>,
+    modifier: Modifier = Modifier
+){
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationsCard(
+                affirmations = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationsCard(
+    affirmations: Affirmations, 
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmations.imageResourceId),
+                contentDescription = stringResource(id = affirmations.stringResourceId),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp)
+            )
+            Text(
+                text = LocalContext.current.getString(affirmations.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+                )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AffirmationCardPreview() {
+    AffirmationsCard(Affirmations(R.string.affirmation1,R.drawable.image1))
 }
---
+++ app/src/main/java/com/lavrent/affirmationscompose/model/Affirmations.kt
@@ -0,0 +1,9 @@
+package com.lavrent.affirmationscompose.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmations(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
